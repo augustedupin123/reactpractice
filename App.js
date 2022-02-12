c72aae2d73b95f98b4fb8588ec4a1895b29d3cab
augustedupin123 <47938823+augustedupin123@users.noreply.github.com>
2022-02-13 02:26:29 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+// import ReactDOM from 'react-dom';
+import Heading from './Heading';
+import Para from './para';
+function App(){
+  return(
+    <>
+    <Heading />
+    <Para />
+  <h1>This is city Bangalore</h1>
+  <p>This is karnataka state</p>
+  <ol>
+    <li>bangalore</li>
+    <li>mangalore</li>
+    <li>chikmangalur</li>
+    <li>akshaynagar</li>
+  </ol>
+  </>
+  );
+}
+export default App;
---
+++ Heading.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+function Heading(){
+    return(
+        <>
+        <h1>react is what is the best framework</h1>
+        <p>the weather is good tonight</p>
+        </>
+    );
+}
+export default Heading;
