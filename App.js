9c58ed3e37d73c130ba63e43bc82cad18e38502b
augustedupin123 <47938823+augustedupin123@users.noreply.github.com>
2022-02-14 22:37:12 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,15 +1,33 @@
-import React from 'react';
-import {add,divi,sub,mult} from './calc';
-function App(){
-  return(
-    <>
-      <ul>
-      <li>The sum of the two nos. is{add(40,4)}</li>
-      <li>The division of the two nos. is{divi(40,4)}</li>
-      <li>The subtraction of the two nos. is{sub(40,4)}</li>
-      <li>The multiplication of the two nos. is {mult(40,4)}</li>
-    </ul>
-    </>
-  );
+ import React from 'react';
+ const arrypractice = [1,2,3,4,5,6];
+//  inside the parantheses {} we can put javascript null , numbers (3+2), strings, arrays. 
+
+//  class MyComponent extends React.component{
+
+//  }
+function IAmH1(){
+  return (
+    <div>
+      <h1>hello world</h1>
+      <h2>wow all this is so cool {["a1", "a2"]}</h2>
+      <h3>{arrypractice}</h3>
+      <h4>{arrypractice.map(val => val ** 2)}</h4>
+      
+      <h4>{arrypractice.map(val => <div>{val ** 2}</div>)}</h4>
+      <h4>{arrypractice.map(val => <div>val ** 2</div>)}</h4>
+    </div>
+  )
+    
+    
+    
+  
 }
-export default App;
+ function App(){
+   return (
+     <div><IAmH1 /></div>
+     
+     
+     
+   )
+ }
+//  export default App;
