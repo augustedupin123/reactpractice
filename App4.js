d092e208dc2b31435201e3ca680ad4779bc1fafa
augustedupin123 <47938823+augustedupin123@users.noreply.github.com>
2022-02-17 00:41:04 +0530

    Add files via upload

---
+++ App4.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+
+function App4(){
+    const [name, setName] = useState('')
+    const [password, setPassword] = useState('')
+     
+    return (
+        <div>
+            <input type="text" value={name} onChange={updateTextField} />
+            <input type="password" value={password} onChange={updatepasswordField} />
+            <button onClick = {submitForm}>Submit Form</button>
+        </div>
+    )
+    if(name === 'admin'){
+        console.log('bhapp');
+    }
+    function submitForm(){
+        console.log(name, password);
+        alert('afridi');
+    }
+    function updateTextField(event){
+        setName(event.target.value);
+    }
+    function updatepasswordField(event){
+        setPassword(event.target.password);
+    }
+    
+}
+export default App4;
