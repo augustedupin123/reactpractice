601d6470c651925f05f0a1cb653892fd39a20d14
augustedupin123 <47938823+augustedupin123@users.noreply.github.com>
2022-02-16 18:09:34 +0530

    Add files via upload

---
+++ avg_marks.js
@@ -0,0 +1,10 @@
+var stu_1 = 10;
+var stu_2 = 20;
+var stu_3 = 30;
+var stu_4 = 40;
+var stu_5 = 50;
+var stu_6 = 60;
+
+var avg;
+avg = (stu_1 + stu_2 + stu_3 + stu_4 + stu_5 + stu_6)/6;
+console.log(avg);
