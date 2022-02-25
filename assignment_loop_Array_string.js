76ac9353c83b67c9a1e4fd8cf78277b20847506c
augustedupin123 <47938823+augustedupin123@users.noreply.github.com>
2022-02-26 02:50:40 +0530

    Add files via upload

---
+++ assignment_loop_Array_string.js
@@ -0,0 +1,21 @@
+var s = 20;
+for(var k=0; k<=20; k++){
+    console.log("Masai School");
+}
+
+var w = 100;
+var a1 = [];
+for(var b = 0; b<100; b++){
+    if(b%3==0){
+        a1.push(b);
+    }
+}
+console.log(a1);
+sum1 = 0;
+
+for (var j = 0; j<a1.length; j++){
+    sum1 = sum1 + a1[j];
+}
+console.log(sum1);
+console.log(sum1/a1.length);
+
