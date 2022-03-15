36722968c8b960a9c7e9f62321d2e10ec9d22d47
augustedupin123 <47938823+augustedupin123@users.noreply.github.com>
2022-03-15 14:32:24 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,33 +1,21 @@
- import React from 'react';
- const arrypractice = [1,2,3,4,5,6];
-//  inside the parantheses {} we can put javascript null , numbers (3+2), strings, arrays. 
-
-//  class MyComponent extends React.component{
-
-//  }
-function IAmH1(){
-  return (
-    <div>
-      <h1>hello world</h1>
-      <h2>wow all this is so cool {["a1", "a2"]}</h2>
-      <h3>{arrypractice}</h3>
-      <h4>{arrypractice.map(val => val ** 2)}</h4>
-      
-      <h4>{arrypractice.map(val => <div>{val ** 2}</div>)}</h4>
-      <h4>{arrypractice.map(val => <div>val ** 2</div>)}</h4>
-    </div>
-  )
-    
-    
-    
-  
+import React from 'react';
+// import ReactDOM from 'react-dom';
+import Heading from './Heading';
+import Para from './para';
+function App(){
+  return(
+    <>
+    <Heading />
+    <Para />
+  <h1>This is city Bangalore</h1>
+  <p>This is karnataka state</p>
+  <ol>
+    <li>bangalore</li>
+    <li>mangalore</li>
+    <li>chikmangalur</li>
+    <li>akshaynagar</li>
+  </ol>
+  </>
+  );
 }
- function App(){
-   return (
-     <div><IAmH1 /></div>
-     
-     
-     
-   )
- }
 export default App;
