9365d307df040a8bd55e79d85fa4506b689e2f2f
augustedupin123 <47938823+augustedupin123@users.noreply.github.com>
2022-03-21 08:43:37 +0530

    Add files via upload

---
+++ switchcase.js
@@ -0,0 +1,18 @@
+var month = 3;
+
+if(month == 1){
+    console.log("jan");
+} else if (month == 2) {
+    console.log("feb");
+}
+
+switch (month) {
+    case 1:
+        console.log("jan");
+        break;
+    case 2:
+        console.log("feb");
+    case 3:
+        console.log("mar");
+    
+}
