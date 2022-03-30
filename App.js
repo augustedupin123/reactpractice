f6d3ab0837046fb163ebaeeb0bc2f38f7eba37fe
augustedupin123 <47938823+augustedupin123@users.noreply.github.com>
2022-03-30 08:50:34 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,21 +1,6 @@
 import React from 'react';
-// import ReactDOM from 'react-dom';
-import Heading from './Heading';
-import Para from './para';
-function App(){
-  return(
-    <>
-    <Heading />
-    <Para />
-  <h1>This is city Bangalore</h1>
-  <p>This is karnataka state</p>
-  <ol>
-    <li>bangalore</li>
-    <li>mangalore</li>
-    <li>chikmangalur</li>
-    <li>akshaynagar</li>
-  </ol>
-  </>
-  );
+import Restaurant from "./components/basics/Restaurant";
+const App = ()=>{
+  return <Restaurant />
 }
 export default App;
