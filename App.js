57fa8f2fd3e3c840aaabcc5d47a665a0c8f929f2
augustedupin123 <47938823+augustedupin123@users.noreply.github.com>
2022-04-04 14:20:24 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,6 +1,11 @@
-import React from 'react';
-import Restaurant from "./components/basics/Restaurant";
-const App = ()=>{
-  return <Restaurant />
+
+
+function App() {
+  return (
+    <div className="App">
+      <h2>welcome to React JS</h2>
+    </div>
+  );
 }
+
 export default App;
