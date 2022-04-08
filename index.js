fcef8510a5252546d502cd834355308cbc6ef755
augustedupin123 <47938823+augustedupin123@users.noreply.github.com>
2022-04-08 14:45:36 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,29 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-
-import Greet from './Greet';
-import Greet1 from './Greet1';
-import Welcome from './welcome';
-import Hello from './Hello1';
+import './index.css';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Greet />
-    <Greet1 name="harry" superhero = 'ironMan'>
-      <p>This is sidekick prop</p>
-    </Greet1>
-    <Greet1 name="meghan" superhero = 'hee-man'>
-      <button>Action</button>
-    </Greet1>
-    <Greet1 name="andrew"superhero = 'spiderman'/>
-    
-    <Welcome name="andrew"superhero = 'spiderman'/>
-    <Welcome name="harry" superhero = 'ironMan'/>
-    <Welcome name="meghan" superhero = 'hee-man'/>
-    <Hello />
-    
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
