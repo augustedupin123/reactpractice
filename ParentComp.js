629fdfb316333dc019002ba9c71ebce28edebb56
augustedupin123 <47938823+augustedupin123@users.noreply.github.com>
2022-04-09 18:32:54 +0530

    Add files via upload

---
+++ ParentComp.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import PureComp from './PureComp';
+import RegComp from './RegComp';
+
+class ParentComp extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            name: 'monroe',
+        }
+    }
+    componentDidMount(){
+        setInterval(()=>{
+            this.setState({
+                name: 'monroe'
+            })
+        }, 2000);
+    }
+    render() {
+        console.log('parent component render');
+        return (
+            <div>
+                Parent Component.
+                <RegComp name={this.state.name}/>
+                <PureComp name={this.state.name}/>
+            </div>
+        );
+    }
+}
+
+export default ParentComp;
