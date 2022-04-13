50ae6a4c9ac936d8968f35589414e5e445d98678
augustedupin123 <47938823+augustedupin123@users.noreply.github.com>
2022-04-13 18:57:00 +0530

    Add files via upload

--- practice.js
+++ practice.js
@@ -1,37 +1,32 @@
-arr_practice = [
-    {
-        "id": 72,
-        "email": "DFuller@id.net",
-        "username": "WHughes",
-        "password": "trpfv"
-    },
-    {
-        "id": 73,
-        "email": "TGage@malesuada.org",
-        "username": "TBeringer",
-        "password": "jPGgb"
-    },
-    {
-        "id": 74,
-        "email": "LHencmann@malesuada.ly",
-        "username": "ADority",
-        "password": "aF5fS"
-    },
-    {
-        "id": 75,
-        "email": "RBozzalla@porta.ly",
-        "username": "HMoccio",
-        "password": "ijIoS"
-    },
-    {
-        "id": 76,
-        "email": "BSweeney@dolor.com",
-        "username": "FAlunde",
-        "password": "LYsNz"
-    }
-]
+import React from 'react'
 
-for (var i=0; i<arr_practice.length; i++) {
-    console.log(arr_practice[i]);
+const withCounter = (WrappedComponent, incrementNumber) => {
+	class WithCounter extends React.Component {
+		constructor(props) {
+			super(props)
+
+			this.state = {
+				count: 0
+			}
+		}
+
+		incrementCount = () => {
+			this.setState(prevState => {
+				return { count: prevState.count + incrementNumber }
+			})
+		}
+    render() {
+      console.log('HOC', this.props.name)
+			return (
+				<WrappedComponent
+          count={this.state.count}
+          incrementCount={this.incrementCount}
+          {...this.props.name}
+				/>
+			)
+		}
+	}
+	return WithCounter
 }
 
+export default withCounter
