bdba49ed4d48629f5f14b38684449879fcd76ccb
augustedupin123 <47938823+augustedupin123@users.noreply.github.com>
2022-04-22 22:27:06 +0530

    Add files via upload

---
+++ typeof_operator.js
@@ -0,0 +1,8 @@
+let a = 5;
+console.log(typeof(a));
+let s = 'elis';
+console.log(typeof(s));
+let n = undefined;
+console.log(typeof(n));
+// number, string, boolean, null, undefined. A method that has not been assigned any value is undefined. Its value does not exist in the compiler.
+// NULL is the intentional absence of value. It is one of the primitive data types in js.
