d8f07dd8f3c5d67cfc8e1dc4c8cd4b3acb83704c
augustedupin123 <47938823+augustedupin123@users.noreply.github.com>
2022-04-22 22:26:03 +0530

    Add files via upload

---
+++ double_and_triple_equalto_difference.js
@@ -0,0 +1,6 @@
+const number = 1234 
+const stringNumber = '1234'  
+
+console.log(number == stringNumber) //true
+console.log(number === stringNumber)  //false     
+// triple = does not convert the type of the variables to match each other. while double = does.
