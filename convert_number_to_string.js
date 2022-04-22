26a19563722a0f85ef3444f4502204ac795d06ed
augustedupin123 <47938823+augustedupin123@users.noreply.github.com>
2022-04-22 22:25:49 +0530

    Add files via upload

---
+++ convert_number_to_string.js
@@ -0,0 +1,15 @@
+let var1 = 22;
+console.log(typeof(var1));
+var1 = var1 + "";
+console.log(var1);
+console.log(typeof(var1));
+
+let l = 'noam chomsky'
+console.log(typeof(l));
+l = +'noam chomsky';
+console.log(typeof(l));
+console.log(l);
+
+let laptops = "5";
+laptops = Number(5);
+console.log(typeof(laptops));
