2f522efe87407cc01b62c422283efea786a9c7a4
augustedupin123 <47938823+augustedupin123@users.noreply.github.com>
2022-04-29 01:26:06 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-// import './index.css';
-// import App from './App';
-// import reportWebVitals from './reportWebVitals';
-
-ReactDOM.render(
-  <>
-  <App />
-  </>,
-  document.getElementById('root')
-);
-
+export const incNumber = ()=>{
+    return{
+        type: "INCREMENT"
+    }
+}
+export const decNumber = ()=>{
+    return{
+        type: "DECREMENT"
+    }
+}
